36c5f35ac065cc6814e8eea70b0afe3bcdab3f48
Roberts Ozoliņš <47858537+robertsozolins@users.noreply.github.com>
2020-01-01 16:16:19 +0200

    alpha
    alpha

---
+++ demo_post/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+import Image from "./components/svelte-responsive-images/index.svelte"
+import Post from "./components/Post.svelte"
+</script>
+
+<div class="container">
+	<Image src="100" />
+</div>
+
+<Post />
+
+
+<style>
+.container{
+	max-width:1050px;
+	margin: 0 auto;
+}
+</style>
+
+
+
