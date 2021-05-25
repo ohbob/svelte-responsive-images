5f1c41187641335a98bc9ed23b64f1e6ff582d9d
Roberts Ozoliņš <47858537+ohbob@users.noreply.github.com>
2021-05-25 16:05:05 +0300

    avif
    avif support, rewrite for folder structure.

--- runtime/Image.svelte
+++ runtime/Image.svelte
@@ -1,52 +1,48 @@
-<script>
-    import getImageSrc from './getimg.js'
-    import { fade } from 'svelte/transition'
-    export let w = "100%"
-    export let c = "";
-
-    let loaded = false;
-    export let src = "";
-    let containerWidth
-
-    function load(img) {
-        img.onload = () => loaded = true;
-    }
-</script>
-
 <div class:loaded style="width: 100%; max-width:{w}">
-    <div style="overflow: hidden;" bind:offsetWidth={containerWidth}>
-        {#if !loaded}
-        <img out:fade="{{duration: 300}}" class="placeholder blur" src="./images/jpg/{src}-10.jpg" alt="placeholder">
-			{/if}
-			<picture>
-				<source type="image/webp" srcset={getImageSrc({src}, "webp" , {containerWidth})}>
-				<source srcset={getImageSrc({src}, "jpg" , {containerWidth})}>
-				<img use:load class="main {c}" alt="alt" srcset="./images/jpg/{src}-10.jpg"
-				    onerror="this.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMszXpSDwAFYwJEhxButQAAAABJRU5ErkJggg==';"
-				    loading="lazy">
-			</picture>
-		</div>
+	<div style="overflow: hidden;" bind:offsetWidth={containerWidth}>
+		<picture>
+			<source type="image/avif" srcSet={getImageSrc({src}, "avif" , {containerWidth})}>
+			<source type="image/webp" srcSet={getImageSrc({src}, "webp" , {containerWidth})}>
+			<source srcSet={getImageSrc({src}, "jpg" , {containerWidth})}>
+			<img use:load className="main {c}" alt="Image" src="/assets/images/{src}/{src}_10.webp" loading="lazy">
+		</picture>
+	</div>
 </div>
 
+<script>
+	import getImageSrc from './getimg.js'
+
+	export let w = "100%"
+	export let c = "";
+	let loaded = false;
+	export let src = "";
+	let containerWidth
+
+	function load(img) {
+		img.onload = () => loaded = true;
+	}
+</script>
 
 <style>
-    img {
-        object-position: center;
-        width: 100%;
-        will-change: opacity;
-    }
+	img {
+		object-position: center;
+		width: 100%;
+		will-change: opacity;
+	}
 
-    .blur {
-        filter: blur(1px);
-        transform: scale(1);
-    }
+	.blur {
+		filter: blur(1px);
+		transform: scale(1);
+		/*background: red;*/
+	}
 
-    .placeholder {
-        position: relative;
-        z-index: 1
-    }
+	.placeholder {
+		position: relative;
+		z-index: 1
+	}
 
-    .loaded .placeholder {
-        position: absolute
-    }
+	.loaded .placeholder {
+		position: absolute;
+		/*opacity:0;*/
+	}
 </style>
--- runtime/getimg.js
+++ runtime/getimg.js
@@ -1,40 +1,15 @@
-    export default (imageName, imageFormat, maxSize) => {
-        let setsrc = [50, 100, 200,
-            320,
-            420,
-            520,
-            620,
-            720,
-            920,
-            1020,
-            1220,
-            1420,
-            1920,
-            1920,
-            1920
-        ]
-        let sizes = [25, 50, 100,
-            200,
-            320,
-            420,
-            520,
-            620,
-            720,
-            920,
-            1020,
-            1220,
-            1420,
-            1920
-        ]
-        let str = ""
-        let i = 0
-        sizes.forEach(currentSize => {
-            if ((currentSize) <= maxSize.containerWidth) {
-                str = str.concat(
-                    `./images/${imageFormat}/${imageName.src}-${setsrc[i]}.${imageFormat} ${currentSize}w, `
-                )
-            }
-            i++
-        })
-        return str
-    }
+export default (imageName, imageFormat, maxSize) => {
+    let setsrc = [50, 100, 200, 320, 420, 520, 620, 720, 920, 1020, 1220, 1420, 1920, 1920, 1920, 1920]
+    let sizes = [25, 50, 100, 200, 320, 420, 520, 620, 720, 920, 1020, 1220, 1420, 1920, 1920]
+    let str = ""
+    let i = 0
+    sizes.forEach(currentSize => {
+        if ((currentSize) <= maxSize.containerWidth) {
+            str = str.concat(
+                `/assets/images/${imageName.src}/${imageName.src}_${setsrc[i]}.${imageFormat} ${currentSize}w, `
+            )
+        }
+        i++
+    })
+    return str
+}
