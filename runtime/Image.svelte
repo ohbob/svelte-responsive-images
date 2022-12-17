c48efdc793747d238df1a7c2c1d0126da0c10841
Camo <roberts@nallus.com>
2022-12-17 19:06:53 +0200

    Update Image.svelte

--- runtime/Image.svelte
+++ runtime/Image.svelte
@@ -1,48 +1,79 @@
-<div class:loaded style="width: 100%; max-width:{w}">
-	<div style="overflow: hidden;" bind:offsetWidth={containerWidth}>
-		<picture>
-			<source type="image/avif" srcSet={getImageSrc({src}, "avif" , {containerWidth})}>
-			<source type="image/webp" srcSet={getImageSrc({src}, "webp" , {containerWidth})}>
-			<source srcSet={getImageSrc({src}, "jpg" , {containerWidth})}>
-			<img use:load className="main {c}" alt="Image" src="/assets/images/{src}/{src}_10.webp" loading="lazy">
-		</picture>
-	</div>
-</div>
-
 <script>
-	import getImageSrc from './getimg.js'
+  const optimizedFolder = "optim";
+  export let w = "100%";
+  export let c = "";
+  export let src = "";
+  export let alt = "Image";
+  let loaded = false;
+  src = src.split(".")[0];
+  let containerWidth;
 
-	export let w = "100%"
-	export let c = "";
-	let loaded = false;
-	export let src = "";
-	let containerWidth
+  const SETSRC = [
+    25, 50, 100, 200, 320, 420, 520, 620, 720, 920, 1020, 1220, 1420, 1920,
+    1920, 1920, 1920,
+  ];
+  const SIZES = [
+    10, 25, 50, 100, 200, 320, 420, 520, 620, 720, 920, 1020, 1220, 1420, 1920,
+    1920,
+  ];
 
-	function load(img) {
-		img.onload = () => loaded = true;
-	}
+  function load(img) {
+    img.onload = () => (loaded = true);
+  }
+
+  function getImageSrc(src, imageFormat, maxSize) {
+    let str = "";
+    for (let i = 0; i < SIZES.length; i++) {
+      const currentSize = SIZES[i];
+      if (currentSize <= maxSize.containerWidth) {
+        str += `${optimizedFolder}/${src.src}-${SETSRC[i]}.${imageFormat} ${currentSize}w, `;
+      }
+    }
+    return str;
+  }
 </script>
 
+<div class:loaded style="width: 100%; max-width:{w}">
+  <div style="overflow: hidden;" bind:offsetWidth={containerWidth}>
+    <picture>
+      <source
+        type="image/avif"
+        srcSet={getImageSrc({ src }, "avif", { containerWidth })}
+      />
+      <source
+        type="image/webp"
+        srcSet={getImageSrc({ src }, "webp", { containerWidth })}
+      />
+      <source srcSet={getImageSrc({ src }, "jpg", { containerWidth })} />
+      <img
+        use:load
+        className="main {c}"
+        {alt}
+        src="./{optimizedFolder}/{src}-25.webp"
+        loading="lazy"
+      />
+    </picture>
+  </div>
+</div>
+
 <style>
-	img {
-		object-position: center;
-		width: 100%;
-		will-change: opacity;
-	}
+  img {
+    object-position: center;
+    width: 100%;
+    will-change: opacity;
+  }
 
-	.blur {
-		filter: blur(1px);
-		transform: scale(1);
-		/*background: red;*/
-	}
+  .blur {
+    filter: blur(1px);
+    transform: scale(1);
+  }
 
-	.placeholder {
-		position: relative;
-		z-index: 1
-	}
+  .placeholder {
+    position: relative;
+    z-index: 1;
+  }
 
-	.loaded .placeholder {
-		position: absolute;
-		/*opacity:0;*/
-	}
+  .loaded .placeholder {
+    position: absolute;
+  }
 </style>
